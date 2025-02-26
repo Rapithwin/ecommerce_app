6cc3f547a85e683f757bfc7c90e47fe945cf69f4
unknown <khandanamir7@gmail.com>
2025-02-26 18:43:48 +0330

    Input field style

--- lib/authentication/widgets/auth_form.dart
+++ lib/authentication/widgets/auth_form.dart
@@ -1,1 +1,71 @@
+import 'package:flutter/material.dart';
 
+class LoginForm extends StatelessWidget {
+  const LoginForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Align(
+        alignment: Alignment(0, -1 / 3),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            _UsernameInput(),
+            const Padding(padding: EdgeInsets.all(12)),
+            _PasswordInput(),
+            const Padding(padding: EdgeInsets.all(12)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _UsernameInput extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+      child: TextField(
+        key: const Key("loginForm_usernameInput_textField"),
+        onChanged: (username) {},
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.grey[370],
+          labelText: 'username',
+          floatingLabelBehavior: FloatingLabelBehavior.never,
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _PasswordInput extends StatelessWidget {
+  const _PasswordInput();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+      child: TextField(
+        key: const Key("loginForm_passwordInput_textField"),
+        onChanged: (password) {},
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.grey[370],
+          labelText: 'password',
+          floatingLabelBehavior: FloatingLabelBehavior.never,
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+      ),
+    );
+  }
+}
