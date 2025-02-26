4b1d80c4f4eb856aedb47a8cfd63cdddd7db1867
unknown <khandanamir7@gmail.com>
2025-02-26 19:27:02 +0330

    App theme created

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart' show Color;
 
 class AppColors {
   static const Color inputFieldColor = Color(0x449E9E9E);
-  static const Color pirmaryColor = Color(0xffffffff);
-  static const Color secondayColor = Color(0x0e0e0eff);
+  static const Color whiteColor = Color(0xffffffff);
+  static const Color blackColor = Color(0xff0e0e0e);
+  static const Color tertiaryColor = Color(0xff9eb9c4);
 }
