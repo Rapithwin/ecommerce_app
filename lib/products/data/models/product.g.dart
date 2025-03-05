a92c3437da78500df616eaff226ad3fe8d2ba4b6
unknown <khandanamir7@gmail.com>
2025-03-05 21:45:23 +0330

    Auto generated fromJson method

---
+++ lib/products/data/models/product.g.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'product.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Product _$ProductFromJson(Map<String, dynamic> json) => Product(
+      id: (json['id'] as num).toInt(),
+      name: json['name'] as String,
+      description: json['description'] as String,
+      price: (json['price'] as num).toInt(),
+    );
