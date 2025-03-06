4732a3ce83fc8af39e41cabc069f65b6abe08379
unknown <khandanamir7@gmail.com>
2025-03-06 10:45:30 +0330

    Mocks and setUps implemented for api test

---
+++ packages/ecommerce_data/test/product_test/prodeuct_api_client_test.dart
@@ -0,0 +1,26 @@
+import 'package:e_commerce/products_data/product_api_client.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:mocktail/mocktail.dart';
+
+class MockHttpClient extends Mock implements http.Client {}
+
+class MockResponse extends Mock implements http.Response {}
+
+class FakeUri extends Fake implements Uri {}
+
+void main() {
+  late http.Client httpClient;
+  late ProductApiClient apiClient;
+
+  group("ProductApiClient", () {
+    setUpAll(() {
+      registerFallbackValue(FakeUri());
+    });
+
+    setUp(() {
+      httpClient = MockHttpClient();
+      apiClient = ProductApiClient(httpClient: httpClient);
+    });
+  });
+}
