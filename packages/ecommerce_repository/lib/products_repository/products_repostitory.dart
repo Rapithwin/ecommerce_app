89a587931f9c75fe2d6a1fab9a3e4606d843901c
unknown <khandanamir7@gmail.com>
2025-03-06 16:37:37 +0330

    Product repository implemented

---
+++ packages/ecommerce_repository/lib/products_repository/products_repostitory.dart
@@ -0,0 +1,22 @@
+import 'package:e_commerce_data/products_data/models/product.dart';
+import 'package:e_commerce_data/products_data/product_api_client.dart';
+
+/// {@template products_repository}
+/// A repository that handles `Product` related requests.
+/// {@endtemplate}
+class ProductsRepostitory {
+  /// {@macro products_repository}
+  ProductsRepostitory({required ProductApiClient productApi})
+      : _productApi = productApi;
+  final ProductApiClient _productApi;
+
+  /// Provides a [List] of all the available products.
+  Future<List<Product>> getProducts() => _productApi.getAllProducts();
+
+  /// Provides a product of a given `id`.
+  Future<Product> getProductById({required int id}) =>
+      _productApi.getProductById(id: id);
+
+  /// Disposes any resources managed by the repository.
+  void dispose() => _productApi.close();
+}
