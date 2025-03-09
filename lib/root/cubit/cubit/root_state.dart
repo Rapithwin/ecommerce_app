29e544cee2c20daf250624ea4f2bd72e8e31e422
unknown <khandanamir7@gmail.com>
2025-03-09 22:21:20 +0330

    Files created

---
+++ lib/root/cubit/cubit/root_state.dart
@@ -0,0 +1,10 @@
+part of 'root_cubit.dart';
+
+sealed class RootState extends Equatable {
+  const RootState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class RootInitial extends RootState {}
