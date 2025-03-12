68aff0b48a35d89b6c82d59af3a4a7c56645fdb9
unknown <khandanamir7@gmail.com>
2025-03-12 16:26:46 +0330

    Cubit test initial state

--- test/products/cubit/products_cubit_test.dart
+++ test/products/cubit/products_cubit_test.dart
@@ -1,6 +1,25 @@
+import 'package:e_commerce/products/presentation/cubit/products_cubit.dart';
 import 'package:e_commerce_repository/ecommerce_repository.dart'
     as products_repository show ProductsRepostitory;
 import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
 
 class MockProductsRepository extends Mock
     implements products_repository.ProductsRepostitory {}
+
+void main() {
+  group("ProductsCubit", () {
+    late products_repository.ProductsRepostitory repository;
+    late ProductsCubit cubit;
+
+    setUp(() {
+      repository = MockProductsRepository();
+      cubit = ProductsCubit(repository);
+    });
+
+    test("Initial state is correct", () {
+      cubit = ProductsCubit(repository);
+      expect(cubit.state, ProductsState());
+    });
+  });
+}
