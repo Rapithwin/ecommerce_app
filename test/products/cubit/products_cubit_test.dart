6404eae839aa247d39a891a1ad1dfee447344f4c
unknown <khandanamir7@gmail.com>
2025-03-12 17:00:49 +0330

    Cubit test fetchProducts failure

--- test/products/cubit/products_cubit_test.dart
+++ test/products/cubit/products_cubit_test.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:e_commerce/products/presentation/cubit/products_cubit.dart';
 import 'package:e_commerce_repository/ecommerce_repository.dart'
     as products_repository show ProductsRepostitory;
@@ -20,6 +21,21 @@ void main() {
     test("Initial state is correct", () {
       cubit = ProductsCubit(repository);
       expect(cubit.state, ProductsState());
+    });
+
+    group("fetchProducts", () {
+      blocTest<ProductsCubit, ProductsState>(
+        'emits [loading, failure] when fetchProducts throws.',
+        setUp: () {
+          when(() => repository.getProducts()).thenThrow(Exception("oops"));
+        },
+        build: () => cubit,
+        act: (cubit) => cubit.fetchProducts(),
+        expect: () => <ProductsState>[
+          ProductsState(status: ProductsStatus.loading),
+          ProductsState(status: ProductsStatus.failure),
+        ],
+      );
     });
   });
 }
