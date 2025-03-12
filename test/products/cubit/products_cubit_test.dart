c9e4d8af0271e1f97ca09df1803bbf068a7f16c8
unknown <khandanamir7@gmail.com>
2025-03-12 18:48:54 +0330

    Cubit test fetchProductsById success

--- test/products/cubit/products_cubit_test.dart
+++ test/products/cubit/products_cubit_test.dart
@@ -78,6 +78,31 @@ void main() {
           ProductsState(status: ProductsStatus.failure),
         ],
       );
+      blocTest<ProductsCubit, ProductsState>(
+        'emits [loading, success] when fetchProductsById returns.',
+        setUp: () {
+          when(() => repository.getProductById(id: 1)).thenAnswer((_) async =>
+              Product(
+                  id: 1, name: "test", description: "test product", price: 1));
+        },
+        build: () => cubit,
+        act: (cubit) => cubit.fetchProductsById(id: 1),
+        expect: () => <dynamic>[
+          ProductsState(status: ProductsStatus.loading),
+          isA<ProductsState>()
+              .having((p) => p.status, "status", ProductsStatus.success)
+              .having(
+                (p) => p.products,
+                "products",
+                isA<List<Product>>()
+                    .having((p) => p[0].id, "id", 1)
+                    .having(
+                        (p) => p[0].description, "description", "test product")
+                    .having((p) => p[0].name, "name", "test")
+                    .having((p) => p[0].price, "price", 1),
+              )
+        ],
+      );
     });
   });
 }
