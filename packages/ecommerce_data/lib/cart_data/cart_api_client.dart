9f0690322e70a906dbf07a10821102fc0db8c250
unknown <khandanamir7@gmail.com>
2025-03-12 21:41:47 +0330

    Cart api client get cart for user id

--- packages/ecommerce_data/lib/cart_data/cart_api_client.dart
+++ packages/ecommerce_data/lib/cart_data/cart_api_client.dart
@@ -1,1 +1,33 @@
+import 'dart:convert';
 
+import 'package:e_commerce_data/cart_data/models/cart.dart';
+import 'package:e_commerce_data/data_constants.dart';
+import 'package:http/http.dart' as http;
+
+/// Exception thrown when cart list is empty.
+class CartEmptyFailure implements Exception {}
+
+/// Exception thrown when getCart fails.
+class CartRequestFailure implements Exception {}
+
+class CartApiClient {
+  CartApiClient({http.Client? httpClient})
+      : _httpClient = httpClient ?? http.Client();
+  final http.Client _httpClient;
+
+  final _cartEndpoint = "api/Carts";
+
+  Future<Cart> getCart({required String userId}) async {
+    final cartRequest =
+        Uri.http(Constants.baseUrlStore, "$_cartEndpoint/$userId");
+
+    final cartResponse = await _httpClient.get(cartRequest);
+
+    if (cartResponse.statusCode != 200) throw CartRequestFailure();
+
+    final cart = jsonDecode(cartResponse.body);
+    final result = Cart.fromJson(cart);
+
+    return result;
+  }
+}
