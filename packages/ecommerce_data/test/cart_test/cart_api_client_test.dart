ea2ba55efe87dc46f29e305ff65a1b9b0e1045bd
unknown <khandanamir7@gmail.com>
2025-03-13 17:38:57 +0330

    getCart api test

--- packages/ecommerce_data/test/cart_test/cart_api_client_test.dart
+++ packages/ecommerce_data/test/cart_test/cart_api_client_test.dart
@@ -1,8 +1,6 @@
 import 'package:e_commerce_data/cart_data/cart_api_client.dart';
 import 'package:e_commerce_data/cart_data/models/cart.dart';
 import 'package:e_commerce_data/data_constants.dart';
-import 'package:e_commerce_data/products_data/models/product.dart';
-import 'package:e_commerce_data/products_data/product_api_client.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:mocktail/mocktail.dart';
@@ -33,7 +31,7 @@ void main() {
       });
     });
 
-    group("getAllProducts", () {
+    group("getCart", () {
       test("Makes correct http request.", () async {
         final response = MockResponse();
         when(() => response.statusCode).thenReturn(200);
@@ -113,68 +111,5 @@ void main() {
         expect(actual, matcher);
       });
     });
-
-//     group("getProductById", () {
-//       const int id = 1;
-//       test("Makes correct http request.", () async {
-//         final response = MockResponse();
-//         when(() => response.statusCode).thenReturn(200);
-//         when(() => response.body).thenReturn("{}");
-//         when(() => httpClient.get(any())).thenAnswer((_) async => response);
-//         try {
-//           await apiClient.getProductById(id: id);
-//         } catch (_) {}
-//         verify(
-//           () => httpClient.get(
-//             Uri.http(
-//               Constants.baseUrlStore,
-//               "api/Products/$id",
-//             ),
-//           ),
-//         ).called(1);
-//       });
-
-//       test("Throws ProductsRequestFailure on non 200 response", () async {
-//         final response = MockResponse();
-//         when(() => response.statusCode).thenReturn(400);
-//         when(() => httpClient.get(any())).thenAnswer((_) async => response);
-//         await expectLater(
-//           apiClient.getProductById(id: id),
-//           throwsA(isA<ProductsRequestFailure>()),
-//         );
-//       });
-
-//       test("Throws ProductNotFoundFailure on error", () async {
-//         final response = MockResponse();
-//         when(() => response.statusCode).thenReturn(200);
-//         when(() => response.body).thenReturn("{}");
-//         when(() => httpClient.get(any())).thenAnswer((_) async => response);
-//         await expectLater(
-//           apiClient.getProductById(id: id),
-//           throwsA(isA<ProductNotFoundFailure>()),
-//         );
-//       });
-
-//       test("Returns products on valid response", () async {
-//         final response = MockResponse();
-//         when(() => response.statusCode).thenReturn(200);
-//         when(() => response.body).thenReturn('''
-//     {
-//       "id": 1,
-//       "name": "test",
-//       "description": "this is a test.",
-//       "price": 1
-//     }
-// ''');
-//         when(() => httpClient.get(any())).thenAnswer((_) async => response);
-//         final actual = await apiClient.getProductById(id: id);
-//         final matcher = isA<Product>()
-//             .having((e) => e.id, "id", 1)
-//             .having((e) => e.name, "name", "test")
-//             .having((e) => e.description, "description", "this is a test.")
-//             .having((e) => e.price, "price", 1);
-//         expect(actual, matcher);
-//       });
-//     });
   });
 }
