dd62abd63f8977a1b310fa397b5c95cc4bd616d8
unknown <khandanamir7@gmail.com>
2025-03-15 12:59:49 +0330

    Cart repository implemented

---
+++ packages/ecommerce_repository/lib/cart_repository/cart_repository.dart
@@ -0,0 +1,23 @@
+import 'package:e_commerce_data/cart_data/cart_api_client.dart';
+import 'package:e_commerce_data/cart_data/models/cart.dart';
+
+/// {@template cart_repository}
+/// A repository that handles `Cart` related requests.
+/// {@endtemplate}
+class CartRepository {
+  final CartApiClient _cartApi;
+
+  /// {@macro cart_repository}
+  CartRepository({CartApiClient? cartApi})
+      : _cartApi = cartApi ?? CartApiClient();
+
+  /// Provides an instanse of [Cart] which includes the cart
+  /// items.
+  Future<Cart> getCart({required String userId}) =>
+      _cartApi.getCart(userId: userId);
+
+  /// Handles adding items to the cart and returns an
+  /// instance of [Cart].
+  Future<Cart> addToCart({required Cart query}) =>
+      _cartApi.addToCart(query: query);
+}
