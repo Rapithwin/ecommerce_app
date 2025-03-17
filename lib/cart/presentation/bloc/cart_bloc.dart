dc9efc35f8cd5c79777556311b4116e8ee2330bf
unknown <khandanamir7@gmail.com>
2025-03-17 14:58:13 +0330

    Bloc created

--- lib/cart/presentation/bloc/cart_bloc.dart
+++ lib/cart/presentation/bloc/cart_bloc.dart
@@ -1,15 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:e_commerce/cart/presentation/models/cart_model.dart';
 import 'package:e_commerce_data/cart_data/models/cart.dart';
+import 'package:e_commerce_repository/cart_repository/cart_repository.dart';
 import 'package:equatable/equatable.dart';
 
 part 'cart_event.dart';
 part 'cart_state.dart';
 
 class CartBloc extends Bloc<CartEvent, CartState> {
+  CartRepository _cartRepository = CartRepository();
+
   CartBloc() : super(CartInitial()) {
-    on<CartEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<LoadCart>(_onLoad());
+    on<ClearCart>(_onClear());
+    on<AddItemToCart>(_onAddItem());
+    on<RemoveItemFromCart>(_onRemoveItem());
+  }
+  @override
+  Future<void> close() {
+    _cartRepository.dispose();
+    return super.close();
   }
 }
