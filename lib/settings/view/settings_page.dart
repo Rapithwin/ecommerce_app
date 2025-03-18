60443b3597ec5af942ecb675b66a66df6952021f
unknown <khandanamir7@gmail.com>
2025-03-18 16:14:57 +0330

    Container for theme settings creatd

--- lib/settings/view/settings_page.dart
+++ lib/settings/view/settings_page.dart
@@ -11,6 +11,45 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final size = MediaQuery.of(context).size;
+    final theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        children: [
+          Divider(
+            height: 1,
+          ),
+          InkWell(
+            onTap: () {},
+            child: Container(
+              height: 60,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  textDirection: TextDirection.rtl,
+                  children: [
+                    Text(
+                      "حالت نمایش",
+                      style: theme.textTheme.labelLarge,
+                    ),
+                    Icon(
+                      Icons.arrow_back_ios,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Divider(
+            height: 1,
+          ),
+          Container(
+            height: 50,
+          )
+        ],
+      ),
+    );
   }
 }
