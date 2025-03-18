f21b2e6f4340bba038e12da3f73f542d74ae904e
unknown <khandanamir7@gmail.com>
2025-03-18 16:39:22 +0330

    Added slide up panel for now, will configure it later

--- lib/settings/view/settings_page.dart
+++ lib/settings/view/settings_page.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   const SettingsPage._();
 
   static Route<void> route() {
     return MaterialPageRoute(
-      builder: (_) => const SettingsPage._(),
+      builder: (_) => SettingsPage._(),
     );
   }
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  final PanelController _panelController = PanelController();
 
   @override
   Widget build(BuildContext context) {
@@ -15,40 +23,50 @@ class SettingsPage extends StatelessWidget {
     final theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(),
-      body: ListView(
-        children: [
-          Divider(
-            height: 1,
-          ),
-          InkWell(
-            onTap: () {},
-            child: Container(
-              height: 60,
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  textDirection: TextDirection.rtl,
-                  children: [
-                    Text(
-                      "حالت نمایش",
-                      style: theme.textTheme.labelLarge,
-                    ),
-                    Icon(
-                      Icons.arrow_back_ios,
-                    )
-                  ],
+      body: SlidingUpPanel(
+        controller: _panelController,
+        defaultPanelState: PanelState.CLOSED,
+        minHeight: 0,
+        panel: Center(
+          child: Text("panel"),
+        ),
+        body: ListView(
+          children: [
+            Divider(
+              height: 1,
+            ),
+            InkWell(
+              onTap: () {
+                _panelController.open();
+              },
+              child: Container(
+                height: 60,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    textDirection: TextDirection.rtl,
+                    children: [
+                      Text(
+                        "حالت نمایش",
+                        style: theme.textTheme.labelLarge,
+                      ),
+                      Icon(
+                        Icons.arrow_back_ios,
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          Divider(
-            height: 1,
-          ),
-          Container(
-            height: 50,
-          )
-        ],
+            Divider(
+              height: 1,
+            ),
+            Container(
+              height: 50,
+            )
+          ],
+        ),
       ),
     );
   }
