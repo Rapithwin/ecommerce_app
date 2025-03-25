7f34586a674cc062ab78fa5e46fdf954088b9536
unknown <khandanamir7@gmail.com>
2025-03-25 15:54:03 +0330

    Added bloc observer for debugging

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:e_commerce/app.dart';
+import 'package:e_commerce/store_bloc_observer.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
+  Bloc.observer = StoreBlocObserver();
   runApp(const MainApp());
 }
