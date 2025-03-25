ea9b0d8d4b1aaf527f5fa57168b612994206e9fa
unknown <khandanamir7@gmail.com>
2025-03-25 21:42:35 +0330

    Some ui modification

--- lib/products/presentation/widgets/products_success.dart
+++ lib/products/presentation/widgets/products_success.dart
@@ -13,32 +13,49 @@ class ProductsSuccess extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return GridView.builder(
-      itemCount: products.length,
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: GridView.builder(
+        itemCount: products.length,
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 250,
+        ),
+        itemBuilder: (context, index) {
+          return Container(
+            color: Colors.transparent,
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.image,
+                  size: 120,
+                ),
+                Expanded(
+                  child: SizedBox(
+                    width: 180,
+                    child: Center(
+                      child: Text(
+                        products[index].name,
+                        style: theme.textTheme.bodyLarge?.copyWith(
+                          color: theme.colorScheme.onSurface,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    products[index].price.toString(),
+                    style: theme.textTheme.bodyLarge?.copyWith(
+                      color: theme.colorScheme.onSurface,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
+        },
       ),
-      itemBuilder: (context, index) {
-        return Container(
-          color: Colors.white,
-          child: Column(
-            children: <Widget>[
-              Text(
-                products[index].name,
-                style: theme.textTheme.bodyLarge?.copyWith(
-                  color: theme.colorScheme.onSurface,
-                ),
-              ),
-              Text(
-                products[index].price.toString(),
-                style: theme.textTheme.bodyLarge?.copyWith(
-                  color: theme.colorScheme.onSurface,
-                ),
-              )
-            ],
-          ),
-        );
-      },
     );
   }
 }
