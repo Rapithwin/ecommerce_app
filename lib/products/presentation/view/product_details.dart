009e464592f2e655bd9b0d07f6d0408c9835cb9c
unknown <khandanamir7@gmail.com>
2025-04-20 17:00:41 +0330

    Added images for test

--- lib/products/presentation/view/product_details.dart
+++ lib/products/presentation/view/product_details.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:e_commerce_data/products_data/models/product.dart';
 import 'package:flutter/material.dart';
 
@@ -16,6 +17,8 @@ class ProductDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
     return Scaffold(
       appBar: AppBar(
         actions: [
@@ -31,8 +34,37 @@ class ProductDetails extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: Text(product.name),
+        child: Column(
+          children: <Widget>[
+            CarouselSlider.builder(
+              itemCount: 3,
+              itemBuilder: imageBuilder,
+              options: CarouselOptions(
+                viewportFraction: 1,
+              ),
+            )
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget imageBuilder(
+    BuildContext context,
+    int index,
+    int realIndex,
+  ) {
+    final ThemeData theme = Theme.of(context);
+    final Size size = MediaQuery.sizeOf(context);
+
+    final List<String> testNetworkImg = [
+      "https://www.mountaingoatsoftware.com/uploads/blog/2016-09-06-what-is-a-product.png",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9YL_gULeR_w6Vus30JxsM0lGuiHFHmJKG5Q&s",
+      "https://media.istockphoto.com/id/1193723594/photo/fujiyoshida-japan-at-chureito-pagoda-and-mt-fuji-in-the-spring-with-cherry-blossoms.jpg?s=612x612&w=0&k=20&c=O5Oy6Bxa7rJs6eqVu4h85OxDd-yBnUVfJ_cAyt5P6iY=",
+    ];
+
+    return Image(
+      image: NetworkImage(testNetworkImg[index]),
     );
   }
 }
