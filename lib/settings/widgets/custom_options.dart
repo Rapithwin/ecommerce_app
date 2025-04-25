9ef81f20426d87cde918dc67edd626633f2ebd61
unknown <khandanamir7@gmail.com>
2025-04-25 13:12:17 +0330

    Extracted setting options to a separate file

---
+++ lib/settings/widgets/custom_options.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class CustomSettingsOption extends StatelessWidget {
+  const CustomSettingsOption({
+    super.key,
+    required this.theme,
+    required this.title,
+    required this.onTap,
+    required this.icon,
+  });
+
+  final ThemeData theme;
+  final String title;
+  final VoidCallback onTap;
+  final String icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column(
+        children: [
+          InkWell(
+            onTap: onTap,
+            child: SizedBox(
+              height: 60,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                textDirection: TextDirection.rtl,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        title,
+                        style: theme.textTheme.labelLarge?.copyWith(
+                          color: theme.colorScheme.onSurface,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 7,
+                      ),
+                      Image.asset(
+                        icon,
+                        color: theme.colorScheme.onSurface,
+                        width: 35,
+                        height: 35,
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5.0),
+                    child: Icon(
+                      Icons.arrow_back_ios,
+                      size: 21,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Divider(
+            height: 1,
+            color: theme.colorScheme.outlineVariant,
+          ),
+        ],
+      ),
+    );
+  }
+}
