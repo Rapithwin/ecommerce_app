eb64255d5fa3487ddbf993435fc9fea3552880a2
unknown <khandanamir7@gmail.com>
2025-04-27 14:24:01 +0330

    Fixed InkWell splash being bigger than container

--- lib/products/presentation/widgets/products_success.dart
+++ lib/products/presentation/widgets/products_success.dart
@@ -25,72 +25,74 @@ class ProductsSuccess extends StatelessWidget {
           childAspectRatio: 0.7,
         ),
         itemBuilder: (context, index) {
-          return InkWell(
-            borderRadius: BorderRadius.circular(10),
-            onTap: () {
-              Navigator.push(
-                context,
-                ProductDetails.route(products[index]),
-              );
-            },
-            child: Container(
-              margin: EdgeInsets.only(top: 5, bottom: 5),
-              decoration: BoxDecoration(
-                color: Colors.transparent,
-                border: Border.all(
-                  color: theme.colorScheme.outline,
-                  width: 3,
+          return Padding(
+            padding: const EdgeInsets.only(top: 5, bottom: 5),
+            child: InkWell(
+              borderRadius: BorderRadius.circular(10),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  ProductDetails.route(products[index]),
+                );
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  border: Border.all(
+                    color: theme.colorScheme.outline,
+                    width: 3,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                children: <Widget>[
-                  Expanded(
-                    flex: 7,
-                    child: Container(
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        // color: theme.colorScheme.secondaryContainer,
-                        borderRadius: BorderRadius.vertical(
-                          top: Radius.circular(7),
-                        ),
-                      ),
-                      child: Icon(
-                        Icons.image,
-                        size: 120,
-                      ),
-                    ),
-                  ),
-                  Divider(
-                    height: 0,
-                    thickness: 0.5,
-                    color: theme.colorScheme.outlineVariant,
-                  ),
-                  Flexible(
-                    flex: 2,
-                    child: SizedBox(
-                      width: 180,
-                      child: Center(
-                        child: Text(
-                          products[index].name,
-                          style: theme.textTheme.bodyLarge?.copyWith(
-                            color: theme.colorScheme.onSurface,
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 7,
+                      child: Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          // color: theme.colorScheme.secondaryContainer,
+                          borderRadius: BorderRadius.vertical(
+                            top: Radius.circular(7),
                           ),
-                          overflow: TextOverflow.ellipsis,
+                        ),
+                        child: Icon(
+                          Icons.image,
+                          size: 120,
                         ),
                       ),
                     ),
-                  ),
-                  Flexible(
-                    flex: 1,
-                    child: Text(
-                      products[index].price.toString(),
-                      style: theme.textTheme.bodyLarge?.copyWith(
-                        color: theme.colorScheme.onSurface,
+                    Divider(
+                      height: 0,
+                      thickness: 0.5,
+                      color: theme.colorScheme.outlineVariant,
+                    ),
+                    Flexible(
+                      flex: 2,
+                      child: SizedBox(
+                        width: 180,
+                        child: Center(
+                          child: Text(
+                            products[index].name,
+                            style: theme.textTheme.bodyLarge?.copyWith(
+                              color: theme.colorScheme.onSurface,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
                       ),
                     ),
-                  )
-                ],
+                    Flexible(
+                      flex: 1,
+                      child: Text(
+                        products[index].price.toString(),
+                        style: theme.textTheme.bodyLarge?.copyWith(
+                          color: theme.colorScheme.onSurface,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           );
