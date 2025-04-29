8f679c0fd1907c47fb962243f8aeff279b14c6d9
unknown <khandanamir7@gmail.com>
2025-04-29 18:18:50 +0330

    UI for containers that show product in cart info

--- lib/cart/presentation/widgets/cart_loaded.dart
+++ lib/cart/presentation/widgets/cart_loaded.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/cart/presentation/widgets/add_quantity.dart';
 import 'package:flutter/material.dart';
 
 class CartLoaded extends StatelessWidget {
@@ -22,6 +23,69 @@ class CartLoaded extends StatelessWidget {
               ),
               border: Border.all(
                 color: theme.colorScheme.outline,
+              ),
+            ),
+            child: Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      AddQuantity(
+                        minNumber: 0,
+                        maxNumber: 20,
+                        iconSize: 17,
+                        value: 2,
+                        valueChanged: (quantity) {},
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          overlayColor: WidgetStateProperty.all(
+                            theme.colorScheme.outlineVariant,
+                          ),
+                        ),
+                        child: Text(
+                          "حذف",
+                          style: theme.textTheme.labelLarge?.copyWith(
+                            color: Colors.red,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "} تومان",
+                        style: theme.textTheme.bodyLarge?.copyWith(
+                          color: theme.colorScheme.onSurfaceVariant,
+                        ),
+                        textDirection: TextDirection.rtl,
+                      ),
+                    ],
+                  ),
+                  Flexible(
+                    child: SizedBox(
+                      width: 30,
+                    ),
+                  ),
+                  Text(
+                    "محصول فلان",
+                    textDirection: TextDirection.rtl,
+                    style: theme.textTheme.bodyLarge?.copyWith(
+                      color: theme.colorScheme.onSurfaceVariant,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Image(
+                      image: NetworkImage(
+                          "https://static.owayo-cdn.com/newhp/img/productHome/productSeitenansicht/productservice/tshirts_classic_herren_basic_productservice/tshirt_basic_4.jpg"),
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
