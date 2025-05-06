75ace6ad4e2faea01096b37a46c4086a80adc619
unknown <khandanamir7@gmail.com>
2025-05-06 21:12:10 +0330

    Made the appbar widgets responsive for larger screens

--- lib/products/presentation/widgets/app_bar_widgets.dart
+++ lib/products/presentation/widgets/app_bar_widgets.dart
@@ -4,61 +4,85 @@ class AppBarWidgets {
   static List<Widget> appBarActions(Size size, ThemeData theme) {
     final sreachBarController = SearchController();
     return [
-      Expanded(
-        child: Column(
-          spacing: 10,
-          children: [
-            SizedBox(
-              width: size.width,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                child: Align(
-                  alignment: Alignment.centerRight,
-                  child: Text(
-                    "محصولات",
-                    textDirection: TextDirection.rtl,
-                    style: theme.textTheme.headlineLarge?.copyWith(
-                      color: theme.colorScheme.onSurface,
-                    ),
-                  ),
-                ),
+      size.width < 800
+          ? Expanded(
+              child: Column(
+                children: _widgetList(size, theme, sreachBarController),
               ),
-            ),
-            Flexible(
-              child: SizedBox(
-                width: size.width - 30,
-                height: size.height / 19,
-                child: Directionality(
-                  textDirection: TextDirection.rtl,
-                  child: SearchBar(
-                    onTapOutside: (event) {
-                      FocusManager.instance.primaryFocus?.unfocus();
-                    },
-                    controller: sreachBarController,
-                    backgroundColor: WidgetStatePropertyAll(
-                        theme.colorScheme.surfaceContainerHighest),
-                    shadowColor: WidgetStatePropertyAll(Colors.transparent),
-                    leading: IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.search),
-                      splashRadius: 1,
-                    ),
-                    hintText: "جستجو...",
-                    hintStyle: WidgetStatePropertyAll(
-                      theme.textTheme.labelLarge?.copyWith(
-                        color: theme.colorScheme.onSurfaceVariant,
-                      ),
-                    ),
-                    shape: WidgetStatePropertyAll(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                    ),
+            )
+          : Expanded(
+              child: Directionality(
+                textDirection: TextDirection.rtl,
+                child: Row(
+                  children: _widgetList(
+                    size,
+                    theme,
+                    sreachBarController,
+                    flex: (size.width / 250).toInt(),
                   ),
                 ),
               ),
             )
-          ],
+    ];
+  }
+
+  static List<Widget> _widgetList(
+    Size size,
+    ThemeData theme,
+    SearchController sreachBarController, {
+    int? flex,
+  }) {
+    return [
+      Flexible(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          child: Align(
+            alignment: Alignment.centerRight,
+            child: Text(
+              "محصولات",
+              textDirection: TextDirection.rtl,
+              style: theme.textTheme.headlineLarge?.copyWith(
+                color: theme.colorScheme.onSurface,
+              ),
+            ),
+          ),
+        ),
+      ),
+      Flexible(
+        flex: flex ?? 1,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+            height: size.height / 18,
+            child: Directionality(
+              textDirection: TextDirection.rtl,
+              child: SearchBar(
+                onTapOutside: (event) {
+                  FocusManager.instance.primaryFocus?.unfocus();
+                },
+                controller: sreachBarController,
+                backgroundColor: WidgetStatePropertyAll(
+                    theme.colorScheme.surfaceContainerHighest),
+                shadowColor: WidgetStatePropertyAll(Colors.transparent),
+                leading: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.search),
+                  splashRadius: 1,
+                ),
+                hintText: "جستجو...",
+                hintStyle: WidgetStatePropertyAll(
+                  theme.textTheme.labelLarge?.copyWith(
+                    color: theme.colorScheme.onSurfaceVariant,
+                  ),
+                ),
+                shape: WidgetStatePropertyAll(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+            ),
+          ),
         ),
       )
     ];
