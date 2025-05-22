59a9454a2135e14e16ab26a2e212c8b60629eba5
unknown <khandanamir7@gmail.com>
2025-05-22 21:01:25 +0330

    Test for modified model

--- packages/ecommerce_data/test/product_test/product_model_test.dart
+++ packages/ecommerce_data/test/product_test/product_model_test.dart
@@ -1,24 +1,47 @@
 import 'package:e_commerce_data/products_data/models/product.dart';
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group("Product", () {
-    group("fromJson", () {
-      test("returns correct Product object.", () {
-        expect(
-          Product.fromJson(<String, dynamic>{
+  group('ProductData.fromJson', () {
+    test('parses JSON correctly', () {
+      final json = {
+        "id": 1,
+        "name": "Test Product",
+        "description": "A test product",
+        "price": 100,
+        "stockQuantity": 5
+      };
+
+      final productData = ProductData.fromJson(json);
+
+      expect(productData.id, 1);
+      expect(productData.name, "Test Product");
+      expect(productData.description, "A test product");
+      expect(productData.price, 100);
+      expect(productData.stockQuantity, 5);
+    });
+  });
+
+  group('Product.fromJson', () {
+    test('parses JSON with data list correctly', () {
+      final json = {
+        "data": [
+          {
             "id": 1,
-            "name": "test",
-            "description": "this is a test.",
-            "price": 1
-          }),
-          isA<Product>()
-              .having((e) => e.id, "id", 1)
-              .having((e) => e.name, "name", "test")
-              .having((e) => e.description, "description", "this is a test.")
-              .having((e) => e.price, "price", 1),
-        );
-      });
+            "name": "Test Product",
+            "description": "A test product",
+            "price": 100,
+            "stockQuantity": 5
+          }
+        ],
+        "error": null
+      };
+
+      final product = Product.fromJson(json);
+
+      expect(product.data.length, 1);
+      expect(product.data.first.name, "Test Product");
+      expect(product.error, null);
     });
   });
 }
