498258ba3cef56498bf9cbf82195b9ecd9727bba
unknown <khandanamir7@gmail.com>
2025-05-24 18:52:38 +0330

    registerUser api method

--- packages/ecommerce_data/lib/auth_data/auth_api_client.dart
+++ packages/ecommerce_data/lib/auth_data/auth_api_client.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+import 'dart:developer' show log;
+
+import 'package:e_commerce_data/auth_data/models/models.dart';
+import 'package:e_commerce_data/data_constants.dart';
 import 'package:http/http.dart' as http;
 
 class AuthApiClient {
@@ -7,4 +12,28 @@ class AuthApiClient {
   final http.Client _httpClient;
 
   final _usersEndpoint = "api/users";
+
+  Future<RegisterResponseModel> registerUser(UserModel userData) async {
+    try {
+      final registerRequest = Uri.http(
+        Constants.authority,
+        "$_usersEndpoint/register",
+      );
+
+      final registerResponse = await _httpClient.post(
+        registerRequest,
+        body: userData.toJson(),
+      );
+
+      final registerJson =
+          jsonDecode(registerResponse.body) as Map<String, dynamic>;
+      final result = RegisterResponseModel.fromJson(registerJson);
+
+      return result;
+    } catch (error, stacktrace) {
+      log("Error during api call $error");
+      log("Stacktrace $stacktrace");
+      rethrow;
+    }
+  }
 }
