046f2fa81cae0c31d9ee374cba3c5c9cb5d58346
unknown <khandanamir7@gmail.com>
2025-05-24 19:34:13 +0330

    Added get user api method

--- packages/ecommerce_data/lib/auth_data/auth_api_client.dart
+++ packages/ecommerce_data/lib/auth_data/auth_api_client.dart
@@ -39,18 +39,38 @@ class AuthApiClient {
 
   Future<LoginResponseModel> loginUser(UserModel userData) async {
     try {
-      final registerRequest = Uri.http(
+      final loginRequest = Uri.http(
         Constants.authority,
         "$_usersEndpoint/login",
       );
 
       final loginResponse = await _httpClient.post(
-        registerRequest,
+        loginRequest,
         body: userData.toJson(),
       );
 
       final loginJson = jsonDecode(loginResponse.body) as Map<String, dynamic>;
       final result = LoginResponseModel.fromJson(loginJson);
+
+      return result;
+    } catch (error, stacktrace) {
+      log("Error during api call $error");
+      log("Stacktrace $stacktrace");
+      rethrow;
+    }
+  }
+
+  Future<UserModel> getUser() async {
+    try {
+      final userRequest = Uri.http(
+        Constants.authority,
+        "$_usersEndpoint/me",
+      );
+
+      final userResponse = await _httpClient.get(userRequest);
+
+      final userJson = jsonDecode(userResponse.body) as Map<String, dynamic>;
+      final result = UserModel.fromJson(userJson);
 
       return result;
     } catch (error, stacktrace) {
