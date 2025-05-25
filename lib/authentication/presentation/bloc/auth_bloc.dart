11630118f52e8b75efd3ca49117ad22e678fdb7f
unknown <khandanamir7@gmail.com>
2025-05-25 18:14:26 +0330

    I put the auth bloc content inside register bloc by mistake. Fixed

--- lib/authentication/presentation/bloc/auth_bloc.dart
+++ lib/authentication/presentation/bloc/auth_bloc.dart
@@ -1,1 +1,64 @@
+// Events
+import 'dart:async';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+abstract class AuthEvent extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class AppStarted extends AuthEvent {}
+
+class LoggedIn extends AuthEvent {
+  final String token;
+  LoggedIn(this.token);
+
+  @override
+  List<Object?> get props => [token];
+}
+
+class LoggedOut extends AuthEvent {}
+
+// States
+abstract class AuthState {}
+
+class AuthInitial extends AuthState {}
+
+class Authenticated extends AuthState {}
+
+class Unauthenticated extends AuthState {}
+
+// Bloc
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  AuthBloc() : super(AuthInitial()) {
+    on<AppStarted>(_onAppStarted);
+    on<LoggedIn>(_onLoginRequested);
+    on<LoggedOut>(
+      _onLogoutRequested,
+    );
+  }
+
+  FutureOr<void> _onLogoutRequested(event, emit) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove("jwt_token");
+    emit(Unauthenticated());
+  }
+
+  FutureOr<void> _onLoginRequested(event, emit) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("jwt_token", event.token);
+    emit(Authenticated());
+  }
+
+  FutureOr<void> _onAppStarted(event, emit) async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString("jwt_token");
+    if (token != null && token.isNotEmpty) {
+      emit(Authenticated());
+    } else {
+      emit(Unauthenticated());
+    }
+  }
+}
