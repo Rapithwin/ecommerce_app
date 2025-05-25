e124aa7587a1e21c2ea974ebd1d6750491d2134b
unknown <khandanamir7@gmail.com>
2025-05-25 17:58:54 +0330

    Events for auth bloc

--- lib/authentication/presentation/bloc/register_bloc.dart
+++ lib/authentication/presentation/bloc/register_bloc.dart
@@ -1,1 +1,11 @@
+// Events
+abstract class AuthEvent {}
 
+class AppStarted extends AuthEvent {}
+
+class LoggedIn extends AuthEvent {
+  final String token;
+  LoggedIn(this.token);
+}
+
+class LoggedOut extends AuthEvent {}
