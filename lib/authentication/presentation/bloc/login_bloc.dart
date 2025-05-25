6a7ff1da0ae0cf432f17d6c40b92bba80cea88a5
unknown <khandanamir7@gmail.com>
2025-05-25 18:16:57 +0330

    Login events and states created

--- lib/authentication/presentation/bloc/login_bloc.dart
+++ lib/authentication/presentation/bloc/login_bloc.dart
@@ -1,1 +1,37 @@
+import 'package:equatable/equatable.dart';
 
+// Events
+abstract class LoginEvent extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class LoginSubmitted extends LoginEvent {
+  LoginSubmitted(this.email, this.password);
+
+  final String email;
+  final String password;
+
+  @override
+  List<Object> get props => [email, password];
+}
+
+// States
+abstract class LoginState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class LoginInitial extends LoginState {}
+
+class LoginLoading extends LoginState {}
+
+class LoginSuccess extends LoginState {}
+
+class LoginFailure extends LoginState {
+  final String error;
+  LoginFailure(this.error);
+
+  @override
+  List<Object?> get props => [error];
+}
