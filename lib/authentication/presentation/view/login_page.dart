4772b78e61ec63b492aad882ec3a88005b4899a5
unknown <khandanamir7@gmail.com>
2025-05-25 20:38:55 +0330

    Placeholder signup and login pages

--- lib/authentication/presentation/view/login_page.dart
+++ lib/authentication/presentation/view/login_page.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  static Route<LoginPage> route() {
+    return MaterialPageRoute(
+      builder: (_) => LoginPage(),
+    );
+  }
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
