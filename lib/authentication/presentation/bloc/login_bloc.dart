b2c8099c35e203226e78c70727ac8ff7f66755b1
unknown <khandanamir7@gmail.com>
2025-05-25 19:04:22 +0330

    Login bloc implemented

--- lib/authentication/presentation/bloc/login_bloc.dart
+++ lib/authentication/presentation/bloc/login_bloc.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+import 'package:e_commerce/authentication/presentation/bloc/auth_bloc.dart';
+import 'package:e_commerce_data/auth_data/models/models.dart';
 import 'package:e_commerce_repository/ecommerce_repository.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 // Events
 sealed class LoginEvent extends Equatable {
@@ -9,13 +13,12 @@ sealed class LoginEvent extends Equatable {
 }
 
 class LoginSubmitted extends LoginEvent {
-  LoginSubmitted(this.email, this.password);
+  LoginSubmitted(this.userData);
 
-  final String email;
-  final String password;
+  final UserModel userData;
 
   @override
-  List<Object> get props => [email, password];
+  List<Object> get props => [userData];
 }
 
 // States
@@ -36,4 +39,29 @@ class LoginFailure extends LoginState {
 
   @override
   List<Object?> get props => [error];
+}
+
+// Bloc
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final AuthRepository _authRepository;
+  final AuthBloc _authBloc;
+
+  LoginBloc(this._authRepository, this._authBloc) : super(LoginInitial()) {
+    on<LoginSubmitted>(_onLoginSubmitted);
+  }
+
+  FutureOr<void> _onLoginSubmitted(event, emit) async {
+    emit(LoginLoading());
+    try {
+      final loginData = await _authRepository.loginUser(event.userData);
+      if (loginData.isSuccess == true && loginData.data != null) {
+        _authBloc.add(LoggedIn(loginData.data!.token));
+        emit(LoginSuccess());
+      } else {
+        emit(LoginFailure(loginData.error ?? "Unkown error"));
+      }
+    } catch (e) {
+      emit(LoginFailure("Login failed: ${e.toString()}"));
+    }
+  }
 }
