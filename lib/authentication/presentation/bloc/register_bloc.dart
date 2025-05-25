9135550db7abaff15025cbe21b2f15ae639c9b2e
unknown <khandanamir7@gmail.com>
2025-05-25 19:45:10 +0330

    Register bloc implemented

--- lib/authentication/presentation/bloc/register_bloc.dart
+++ lib/authentication/presentation/bloc/register_bloc.dart
@@ -1,5 +1,7 @@
 import 'package:e_commerce_data/auth_data/models/models.dart';
+import 'package:e_commerce_repository/ecommerce_repository.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 sealed class RegisterEvent extends Equatable {
   @override
@@ -33,4 +35,25 @@ class RegisterFailure extends RegisterState {
 
   @override
   List<Object?> get props => [error];
+}
+
+// Bloc
+class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
+  final AuthRepository _authRepository;
+
+  RegisterBloc(this._authRepository) : super(RegisterInitial()) {
+    on<RegisterSubmitted>((event, emit) async {
+      emit(RegisterLoading());
+      try {
+        final registerData = await _authRepository.registerUser(event.userData);
+        if (registerData.isSuccess) {
+          emit(RegisterSuccess());
+        } else {
+          emit(RegisterFailure(registerData.error ?? "Unkown register error"));
+        }
+      } catch (e) {
+        emit(RegisterFailure("Register Failed ${e.toString()}"));
+      }
+    });
+  }
 }
