0f0e35d1337a669a925112521bcff3ca43d17675
unknown <khandanamir7@gmail.com>
2025-05-26 20:24:46 +0330

    Login page UI done

--- lib/authentication/presentation/view/login_page.dart
+++ lib/authentication/presentation/view/login_page.dart
@@ -1,3 +1,5 @@
+import 'package:e_commerce/authentication/presentation/view/signup_page.dart';
+import 'package:e_commerce/profile/presentation/widgets/custom_form_field.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -14,8 +16,93 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  late TextEditingController _emailController, _passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final ThemeData theme = Theme.of(context);
+    final Size size = MediaQuery.sizeOf(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Align(
+          alignment: Alignment.centerRight,
+          child: Text(
+            "ورود",
+            textDirection: TextDirection.rtl,
+            style: theme.textTheme.headlineLarge,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: size.height / 4,
+              ),
+              CustomFormField(
+                labelName: "ایمیل",
+                textDirection: TextDirection.ltr,
+                inputAction: TextInputAction.next,
+                controller: _emailController,
+                theme: theme,
+              ),
+              CustomFormField(
+                labelName: "رمز عبور",
+                textDirection: TextDirection.ltr,
+                inputAction: TextInputAction.next,
+                controller: _passwordController,
+                theme: theme,
+              ),
+              Container(
+                width: size.width,
+                margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "ورود",
+                    style: theme.textTheme.labelLarge?.copyWith(
+                      color: theme.colorScheme.onSurface,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pushReplacement(
+                  context,
+                  SignupPage.route(),
+                ),
+                style: ButtonStyle(
+                  textStyle: WidgetStatePropertyAll(
+                    theme.textTheme.labelLarge?.copyWith(
+                      color: theme.colorScheme.tertiaryFixed,
+                    ),
+                  ),
+                ),
+                child: const Text("ثبت نام"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
