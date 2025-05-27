9d336930a223f3897fabd274d113be16413e7dbf
unknown <khandanamir7@gmail.com>
2025-05-27 19:16:32 +0330

    Added extensions to check email and password validation

---
+++ lib/extensions.dart
@@ -0,0 +1,13 @@
+extension ExtString on String {
+  bool get isEmailValid {
+    final emailRegEx = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    return emailRegEx.hasMatch(this);
+  }
+
+  bool get isPasswordValid {
+    final passwordRegEx =
+        RegExp(r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$");
+    return passwordRegEx.hasMatch(this);
+  }
+}
