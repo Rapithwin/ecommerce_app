cb3eeeb74b7c464f6025af325441be83537180d7
unknown <khandanamir7@gmail.com>
2025-05-28 15:38:09 +0330

    Extension for transalting auth errors

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -10,4 +10,15 @@ extension ExtString on String {
         RegExp(r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$");
     return passwordRegEx.hasMatch(this);
   }
+
+  String get authErrorTranslate {
+    switch (this) {
+      case "Invalid email or password":
+        return "ایمیل یا رمز عبور اشتباه است";
+      case "User with this email already exists":
+        return "کاربر با این ایمیل وجود دارد";
+      default:
+        return "خطای ناشناخته‌ای رخ داده است";
+    }
+  }
 }
