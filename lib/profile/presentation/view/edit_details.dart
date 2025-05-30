22bacb16ad2b33df351a1ba4ae7aa7f47b325e4b
unknown <khandanamir7@gmail.com>
2025-05-30 12:55:44 +0330

    Added updateUserDetails functionality to EditDetailsPage

--- lib/profile/presentation/view/edit_details.dart
+++ lib/profile/presentation/view/edit_details.dart
@@ -1,8 +1,13 @@
+import 'package:e_commerce/authentication/presentation/bloc/auth_bloc.dart';
+import 'package:e_commerce/authentication/presentation/bloc/register_bloc.dart';
+import 'package:e_commerce/extensions.dart';
+import 'package:e_commerce/profile/presentation/bloc/profile_cubit.dart';
 import 'package:e_commerce/profile/presentation/widgets/custom_form_field.dart';
 import 'package:e_commerce_data/auth_data/models/user_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class EditDetailsPage extends StatefulWidget {
+class EditDetailsPage extends StatelessWidget {
   const EditDetailsPage({super.key, required this.userData});
 
   final UserModel userData;
@@ -16,10 +21,23 @@ class EditDetailsPage extends StatefulWidget {
   }
 
   @override
-  State<EditDetailsPage> createState() => _EditDetailsPageState();
+  Widget build(BuildContext context) {
+    return EditDetailsView(
+      userData: userData,
+    );
+  }
 }
 
-class _EditDetailsPageState extends State<EditDetailsPage> {
+class EditDetailsView extends StatefulWidget {
+  const EditDetailsView({super.key, required this.userData});
+
+  final UserModel userData;
+
+  @override
+  State<EditDetailsView> createState() => _EditDetailsViewState();
+}
+
+class _EditDetailsViewState extends State<EditDetailsView> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   late TextEditingController _firstNameController,
@@ -47,6 +65,13 @@ class _EditDetailsPageState extends State<EditDetailsPage> {
     _lastNameController.dispose();
     _firstNameController.dispose();
     super.dispose();
+  }
+
+  String? emptyValidator(String? value) {
+    if (value!.isEmpty) {
+      return "این فیلد نباید خالی باشد";
+    }
+    return null;
   }
 
   @override
@@ -78,6 +103,7 @@ class _EditDetailsPageState extends State<EditDetailsPage> {
                     inputAction: TextInputAction.next,
                     controller: _firstNameController,
                     theme: theme,
+                    validator: emptyValidator,
                   ),
                   CustomFormField(
                     labelName: "نام خانوادگی",
@@ -85,6 +111,7 @@ class _EditDetailsPageState extends State<EditDetailsPage> {
                     inputAction: TextInputAction.next,
                     controller: _lastNameController,
                     theme: theme,
+                    validator: emptyValidator,
                   ),
                   CustomFormField(
                     labelName: "آدرس ایمیل",
@@ -93,6 +120,15 @@ class _EditDetailsPageState extends State<EditDetailsPage> {
                     keyboardType: TextInputType.emailAddress,
                     controller: _emailController,
                     theme: theme,
+                    validator: (String? value) {
+                      if (!value!.isEmailValid) {
+                        return "ایمیل نامعتبر";
+                      }
+                      if (value.isEmpty) {
+                        return "این فیلد نباید خالی باشد";
+                      }
+                      return null;
+                    },
                   ),
                   CustomFormField(
                     labelName: "شماره تماس",
@@ -101,6 +137,7 @@ class _EditDetailsPageState extends State<EditDetailsPage> {
                     keyboardType: TextInputType.phone,
                     controller: _phoneController,
                     theme: theme,
+                    validator: emptyValidator,
                   ),
                   CustomFormField(
                     labelName: "آدرس پستی",
@@ -109,20 +146,63 @@ class _EditDetailsPageState extends State<EditDetailsPage> {
                     keyboardType: TextInputType.text,
                     controller: _addressController,
                     theme: theme,
+                    validator: emptyValidator,
                   ),
                   Container(
                     width: size.width,
                     margin: const EdgeInsets.symmetric(
                         horizontal: 10.0, vertical: 5.0),
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        "ذخیره",
-                        style: theme.textTheme.labelLarge?.copyWith(
-                          color: theme.colorScheme.onSurface,
-                          fontSize: 15,
-                        ),
-                      ),
+                    child: BlocBuilder<ProfileCubit, ProfileState>(
+                      builder: (context, state) {
+                        final authState = context.watch<AuthBloc>().state;
+                        final bool isLoading =
+                            state.status == ProfileStatus.loading;
+                        return ElevatedButton(
+                          onPressed: isLoading
+                              ? null
+                              : () {
+                                  final UserModel userData = UserModel(
+                                    email: _emailController.text,
+                                    firstName: _firstNameController.text,
+                                    lastName: _lastNameController.text,
+                                    address: _addressController.text,
+                                    phoneNumber: _phoneController.text,
+                                  );
+                                  if (!_formKey.currentState!.validate()) {
+                                    return;
+                                  }
+                                  if (authState is Authenticated) {
+                                    context
+                                        .read<ProfileCubit>()
+                                        .updateUserDetails(
+                                            authState.token, userData);
+                                  }
+                                },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            spacing: 10,
+                            children: [
+                              Visibility(
+                                visible: isLoading,
+                                child: const SizedBox(
+                                  height: 15,
+                                  width: 15,
+                                  child: CircularProgressIndicator(
+                                    strokeWidth: 3,
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                "ذخیره",
+                                style: theme.textTheme.labelLarge?.copyWith(
+                                  color: theme.colorScheme.onSurface,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
                     ),
                   )
                 ],
