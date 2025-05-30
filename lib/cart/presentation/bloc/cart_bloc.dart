3ee987186d4bf9524435678856d0395571939664
unknown <khandanamir7@gmail.com>
2025-05-30 16:12:36 +0330

    Method _onLoad implemented

--- lib/cart/presentation/bloc/cart_bloc.dart
+++ lib/cart/presentation/bloc/cart_bloc.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:e_commerce/cart/presentation/models/cart_model.dart';
+import 'package:e_commerce_data/cart_data/cart_api_client.dart';
 import 'package:e_commerce_data/cart_data/models/cart.dart';
 import 'package:e_commerce_repository/cart_repository/cart_repository.dart';
 import 'package:equatable/equatable.dart';
@@ -11,10 +14,24 @@ class CartBloc extends Bloc<CartEvent, CartState> {
   final CartRepository _cartRepository = CartRepository();
 
   CartBloc() : super(CartInitial()) {
-    on<LoadCart>(_onLoad());
+    on<LoadCart>(_onLoad);
     on<AddItemToCart>(_onAddItem());
     on<RemoveItemFromCart>(_onRemoveItem());
   }
+  FutureOr<void> _onLoad(event, emit) async {
+    emit(CartLoading());
+    try {
+      final cartData = await _cartRepository.getCart(event.token);
+      if (cartData.data != null && cartData.data!.items != null) {
+        emit(CartLoaded(items: cartData.data!.items!));
+      }
+    } on CartEmptyFailure {
+      emit(const CartError("cart-empty"));
+    } catch (e) {
+      emit(CartError(e.toString()));
+    }
+  }
+
   @override
   Future<void> close() {
     _cartRepository.dispose();
