448ec8f91f0ad95b58765f15444e08f2b84b76af
unknown <khandanamir7@gmail.com>
2025-05-31 21:20:48 +0330

    getAllComments api method created

---
+++ packages/ecommerce_data/lib/comments_data/comment_api_client.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:e_commerce_data/comments_data/models/comment_model.dart';
+import 'package:e_commerce_data/data_constants.dart';
+import 'package:http/http.dart' as http;
+
+class CommentsEmptyFailure implements Exception {}
+
+class CommentApiClient {
+  CommentApiClient({http.Client? httpClient})
+      : _httpClient = httpClient ?? http.Client();
+
+  final http.Client _httpClient;
+
+  final _commentEndpoint = "api/comments";
+
+  Future<CommentModel> getAllComments(String token, int productId) async {
+    final commentRequest = Uri.http(
+      Constants.authority,
+      "$_commentEndpoint/product/$productId",
+    );
+    try {
+      final commentResponse = await _httpClient.get(
+        commentRequest,
+        headers: {
+          HttpHeaders.authorizationHeader: "Bearer $token",
+          HttpHeaders.contentTypeHeader: "application/json",
+        },
+      );
+      final commentJson =
+          jsonDecode(commentResponse.body) as Map<String, dynamic>;
+      final result = CommentModel.fromJson(commentJson);
+      if (result.data != null && result.data!.comments.isEmpty) {
+        throw CommentsEmptyFailure();
+      }
+      return result;
+    } catch (error, stacktrace) {
+      log("Error during api call $error");
+      log("Stacktrace $stacktrace");
+      rethrow;
+    }
+  }
+}
