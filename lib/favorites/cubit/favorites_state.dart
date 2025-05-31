2ef6911a891fcc30c2bfb18298b3a23c49477e51
unknown <khandanamir7@gmail.com>
2025-05-31 14:10:12 +0330

    State for favorites implemented

--- lib/favorites/cubit/favorites_state.dart
+++ lib/favorites/cubit/favorites_state.dart
@@ -1,10 +1,51 @@
 part of 'favorites_cubit.dart';
 
-sealed class FavoritesState extends Equatable {
-  const FavoritesState();
+enum FavoritesStatus { initial, loading, success, failure }
 
-  @override
-  List<Object> get props => [];
+extension FavoritesStatusX on FavoritesStatus {
+  bool get isInitial => this == FavoritesStatus.initial;
+  bool get isLoading => this == FavoritesStatus.loading;
+  bool get isSuccess => this == FavoritesStatus.success;
+  bool get isFailure => this == FavoritesStatus.failure;
 }
 
-final class FavoritesInitial extends FavoritesState {}
+final class FavoritesState extends Equatable {
+  final FavoritesStatus status;
+  final FavoritesModel favorites;
+  final bool isFavorited;
+  final bool isLoadingMore;
+  final String? error;
+
+  const FavoritesState({
+    this.status = FavoritesStatus.initial,
+    required this.favorites,
+    this.isFavorited = false,
+    this.isLoadingMore = false,
+    this.error,
+  });
+
+  FavoritesState copyWith({
+    FavoritesStatus? status,
+    FavoritesModel? favorites,
+    bool? isFavorited,
+    bool? isLoadingMore,
+    String? error,
+  }) {
+    return FavoritesState(
+      status: status ?? this.status,
+      favorites: favorites ?? this.favorites,
+      isFavorited: isFavorited ?? this.isFavorited,
+      isLoadingMore: isLoadingMore ?? this.isLoadingMore,
+      error: error,
+    );
+  }
+
+  @override
+  List<Object?> get props => [
+        status,
+        favorites,
+        isFavorited,
+        isLoadingMore,
+        error,
+      ];
+}
