9876aefd1aff6f698bc032f297c1410c11de7dd7
unknown <khandanamir7@gmail.com>
2025-05-31 14:35:40 +0330

    Addded new favoried widgets

---
+++ lib/favorites/widgets/favorites_success.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FavoritesSuccess extends StatelessWidget {
+  const FavoritesSuccess({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
