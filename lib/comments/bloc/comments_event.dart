07b8860ce450a29b7e306259380d9413aa3ac91c
unknown <khandanamir7@gmail.com>
2025-06-01 09:04:06 +0330

    Comments event implemented

---
+++ lib/comments/bloc/comments_event.dart
@@ -0,0 +1,50 @@
+part of 'comments_bloc.dart';
+
+sealed class CommentsEvent extends Equatable {
+  const CommentsEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class FetchComments extends CommentsEvent {
+  final String token;
+  final int productId;
+  const FetchComments({required this.token, required this.productId});
+
+  @override
+  List<Object?> get props => [token, productId];
+}
+
+class PostComment extends CommentsEvent {
+  final String token;
+  final String content;
+  final int productId;
+  final int rating;
+  const PostComment({
+    required this.token,
+    required this.content,
+    required this.productId,
+    required this.rating,
+  });
+
+  @override
+  List<Object?> get props => [token, content, productId, rating];
+}
+
+class DeleteComment extends CommentsEvent {
+  final String token;
+  final int commentId;
+  const DeleteComment({required this.token, required this.commentId});
+
+  @override
+  List<Object?> get props => [token, commentId];
+}
+
+class FetchUserComments extends CommentsEvent {
+  final String token;
+  const FetchUserComments({required this.token});
+
+  @override
+  List<Object?> get props => [token];
+}
